ad987a59db0b5fa0a71038eb4f47ca9663a6160c
G1veng <daniil-shishko@mail.ru>
2022-11-07 10:29:45 +0300

    Addded required annotations to CreatePostModel and CreatePostCommentModel

--- DDApp.API/Models/CreatePostCommentModel.cs
+++ DDApp.API/Models/CreatePostCommentModel.cs
@@ -1,8 +1,12 @@
-﻿namespace DDApp.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DDApp.API.Models
 {
     public class CreatePostCommentModel
     {
+        [Required]
         public Guid PostId { get; set; }
+        [Required]
         public string Text { get; set; } = null!;
     }
 }
